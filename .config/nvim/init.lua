5b479eb9593cc79afd78584d87af5d7a2c6afb3e
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-06-10 22:57:09 -0700

    Moving the plugins into a separate location.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -61,25 +61,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-}
-}
 local opts = {}
 
 -- require("vim-options")
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 -- Setup Telescope --
 local builtin = require("telescope.builtin")
@@ -91,7 +76,11 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 -- Treesitter --
 local tsconfig = require("nvim-treesitter.configs")
 tsconfig.setup({
-  ensure_installed = {"lua", "elixir"}, 
+  ensure_installed = {"lua",
+    "elixir",
+    "dockerfile",
+    "make",
+  },
   highlight = { enable = true },
   indent = { enable = true }
 })
