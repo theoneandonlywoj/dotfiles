4f9b41b08b32c730f593add43bae336cb7b65bf3
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-06-10 21:16:03 -0700

    Lazy package manager + catppuccin colorscheme.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,30 @@
--------------------------------------------------
--- WOJCIECH'S NEOVIM CONFIGURATION
--- Neovim website: https://neovim.io/
--- Wojciech's dotfiles: https://github.com/theoneandonlywoj/dotfiles
--------------------------------------------------
+-- Settings --
+local g = vim.g
+local o = vim.o
 
-require("theoneandonlywoj/settings")
+-- System Clipboard --
+o.clipboard = 'unnamedplus'
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+local opts = {}
+
+-- require("vim-options")
+require("lazy").setup(plugins, opts)
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
