d250a031d485bb54a7701519a10643b0e49a2c99
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-06-12 19:28:47 -0700

    Adding relative numbers in the neo tree window.

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -9,8 +9,16 @@ return {
   },
   config = function()
     require("neo-tree").setup({
-      close_if_last_window = true
-    })
+      close_if_last_window = true,
+      event_handlers = {{
+        event = "neo_tree_buffer_enter",
+        handler = function(arg)
+          vim.cmd [[
+            setlocal relativenumber
+          ]]
+        end,
+      }},
+  })
     -- NeoTree (file explorer)
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
     vim.cmd(':Neotree filesystem reveal left')
