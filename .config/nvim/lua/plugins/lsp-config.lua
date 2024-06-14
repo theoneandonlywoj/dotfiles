a6264ce1fc07086ce2012c6e2f6107d14d382864
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-06-14 23:06:05 +0200

    Deleting obsolete lang servers.

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -20,15 +20,6 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
